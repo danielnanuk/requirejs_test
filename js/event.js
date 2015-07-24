c11436d711f6dfdab04dd83175d883c924b42cff
daniel <danielnanuk@gmail.com>
2015-07-24 17:38:06 +0800

    define

--- js/event.js
+++ js/event.js
@@ -1,8 +1,9 @@
-define('event',['jquery'],function($){
+define('event', ['jquery'], function($) {
 	return {
-		fire:function(a){
+		fire: function(a) {
 			console.log($(a).html());
 			console.log("Event fired");
 		}
 	};
 });
+
--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-require(['event','logger'],function(e,l){
+require.config({
+	paths:{
+		jquery:"jquery"//模块名和文件名相同时，可以省略
+	}
+})
+
+
+require(['event', 'logger'], function(e, l) {
 	e.fire("#header");
 	l.debug("Logger Debug");
 });
